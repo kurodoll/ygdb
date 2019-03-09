6429b55c6f6bf5d527e3b08b7c2b3a98c406fca7
kurodoll <kurodoll@protonmail.ch>
2019-03-09 20:18:28 +1300

    Clean

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// *                                                          * // App Init //
+const website_name = '野生GameDB';
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// *                                                           * // Routing //
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: website_name });
 });
 
 module.exports = router;
